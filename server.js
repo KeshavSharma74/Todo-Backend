55c0c6b0d33b8ebc9e26d067368309821816c259
keshavsharma74 <keshav165p@gmail.com>
2025-06-08 16:54:49 +0530

    Setup Completed

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app=express();
+require("dotenv").config()
+
+const port=process.env.PORT 
+
+app.listen(port,()=>{
+    console.log("Server is listening on port : " + port);
+})
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Website Backend is working perfectly</h1>")
+})
